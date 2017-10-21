ec3a0d0d2c82d4db194bcbc48602cf9df11d344a
Fedor <fedorvlvl@gmail.com>
2017-10-21 13:58:19 +0300

    Static function for Tags

--- CartoonNews/Assets/CartoonNews/scripts/forUnits/Bestiariy.cs
+++ CartoonNews/Assets/CartoonNews/scripts/forUnits/Bestiariy.cs
@@ -8,11 +8,25 @@ public class Bestiariy : MonoBehaviour
     public enum namePersonCharacters {Huilo, Cat, OldWoman};// characters
 
     #region Base States for units
-    public enum nameParametersInAnimator {walk, run, jump, die}; // States
+    public enum nameParametersInAnimator {walk, run, jump, die}; // States 
+
+    public enum UTags{dead, Player, UnitNeutrall, enemy};
+
+    public UTags StateTag=UTags.dead;
 
     #endregion
 
-
+    public static string UseTag(UTags StateTag)
+    {
+        switch (StateTag)
+        {
+            case (UTags.dead):return "dead";break; 
+            case (UTags.Player):return "Player";break; 
+            case (UTags.enemy):return "Enemy";break; 
+                
+            default: return "enemy"; break;               
+        }
+    }
 
 	// Use this for initialization
 	void Start () {
